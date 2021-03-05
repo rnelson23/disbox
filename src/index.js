f694ba5f5bed50a1f508bf3a7ffadb1d59fcf5e0
Ellie-exe <ryiscool247@gmail.com>
2021-03-04 19:33:18 -0500

    Core mechanics in place

--- src/index.js
+++ src/index.js
@@ -1,72 +1,85 @@
 const discord = require('discord.js');
-const { MessageEmbed } = require('discord.js');
 const logger = require('@jakeyprime/logger');
 
 const client = new discord.Client();
 const games = new discord.Collection();
 const messages = new discord.Collection();
-const moves = new discord.Collection();
+const counters = new discord.Collection();
 const levels = new discord.Collection();
+const grabbers = new discord.Collection();
 
 client.on('ready', async () => {logger.info('Bot ready')});
 
 client.on('message', async (message) => {
-    const black = ':black_large_square:'
-    const red = ':red_square:'
-    const goal = ':negative_squared_cross_mark:'
-    const block = ':brown_square:'
-    const player = ':flushed:'
+    const black = ':black_large_square:';
+    const red = ':red_square:';
+    const goal = ':negative_squared_cross_mark:';
+    const block = ':brown_square:';
+    const player = ':flushed:';
+    const grabbing = ':weary:';
 
-    async function newGame() {
-        const board = [
-            red,  red,   red,   red,   red,   red,   red,   red,  red, '\n',
-            red, black, black, black, black, black, black, black, red, '\n',
-            red, black, black, black, black, black, black, black, red, '\n',
-            red, black, black, black, black, black, black, black, red, '\n',
-            red, black, black, black, black, black, black, black, red, '\n',
-            red,  red,   red,   red,   red,   red,   red,   red,  red, '\n'
-        ];
+    let board = [];
 
+    async function newGame(level = 1) {
         async function randomize() {
+            board = [
+                red,  red,   red,   red,   red,   red,   red,   red,  red, '\n',
+                red, black, black, black, black, black, black, black, red, '\n',
+                red, black, black, black, black, black, black, black, red, '\n',
+                red, black, black, black, black, black, black, black, red, '\n',
+                red, black, black, black, black, black, black, black, red, '\n',
+                red,  red,   red,   red,   red,   red,   red,   red,  red, '\n'
+            ];
+
             const playerLocation = Math.floor(Math.random() * board.length);
             const goalLocation = Math.floor(Math.random() * board.length);
             const blockLocation = Math.floor(Math.random() * board.length);
-        
+
             if (board[playerLocation] !== black) {
-                randomize();
-                return;
-            }
-        
-            if (board[goalLocation] !== black) {
-                randomize();
-                return;
-            }
-        
-            if (board[blockLocation] !== black) {
-                randomize();
+                await randomize();
                 return;
             }
         
             board.splice(playerLocation, 1, player);
-            board.splice(goalLocation, 1, goal);
-            board.splice(blockLocation, 1, block);
+
+            if (board[goalLocation] !== black) {
+                await randomize();
+                return;
+            }
         
+            board.splice(goalLocation, 1, goal);
+
+            if (board[blockLocation] !== black) {
+                await randomize();
+                return;
+            }
+
+            board.splice(blockLocation, 1, block);
+
             return board;
         }
 
         await randomize();
 
-        const embed = new MessageEmbed()
-            .setAuthor(`Level 1`)
+        const embed = new discord.MessageEmbed()
+            .setAuthor(`Level ${level}`)
             .setDescription(board.join(''))
-            .setFooter('Enter direction (w, a, s, d) or r to reset');
+            .setFooter('Number of moves: 0');
 
-        const start = await message.channel.send(embed);
+        if (level === 1) {
+            const start = await message.channel.send(embed);
+            messages.set(message.author.id, start);
+        
+        } else {
+            const gameMessage = messages.get(message.author.id);
+            await gameMessage.edit(embed);
+        }
 
         games.set(message.author.id, board);
-        messages.set(message.author.id, start);
-        levels.set(message.author.id, 1);
-        
+        levels.set(message.author.id, level);
+        counters.set(message.author.id, 0);
+        grabbers.set(message.author.id, false);
+
         logger.info('Game Start');
     }
 
@@ -74,179 +87,197 @@ client.on('message', async (message) => {
     if (message.content.startsWith(process.env.prefix + 'start')) newGame();
 
     if (games.has(message.author.id)) {
-        const board = games.get(message.author.id);
-        const gameMessage = messages.get(message.author.id);
-        const playerLocation = board.indexOf(player);
-
-        let level = levels.get(message.author.id);
-        let win = false;
-
-        switch (message.content) {
-            case 'w': {
-                if (board[playerLocation - 10] === black) {
-                    board.splice(playerLocation - 10, 1, player);
-                    board.splice(playerLocation, 1, black);
-                }
-
-                if (board[playerLocation - 10] === block && board[playerLocation - 20] !== red) {
-                    if (board[playerLocation - 20] === goal) win = true;
-
-                    board.splice(playerLocation - 20, 1, block);
-                    board.splice(playerLocation - 10, 1, player);
-                    board.splice(playerLocation, 1, black);
-                }
-
-                break;
-            }
-
-            case 'a': {
-                if (board[playerLocation - 1] === black) {
-                    board.splice(playerLocation - 1, 1, player);
-                    board.splice(playerLocation, 1, black);
-                }
-
-                if (board[playerLocation - 1] === block && board[playerLocation - 2] !== red) {
-                    if (board[playerLocation - 2] === goal) win = true;
-    
-                    board.splice(playerLocation - 2, 1, block);
-                    board.splice(playerLocation - 1, 1, player);
-                    board.splice(playerLocation, 1, black);
-                }
-
-                break;
-            }
-
-            case 's': {
-                if (board[playerLocation + 10] === black) {
-                    board.splice(playerLocation + 10, 1, player);
-                    board.splice(playerLocation, 1, black);
-                }
-
-                if (board[playerLocation + 10] === block && board[playerLocation + 20] !== red) {
-                    if (board[playerLocation + 20] === goal) win = true;
-    
-                    board.splice(playerLocation + 20, 1, block);
-                    board.splice(playerLocation + 10, 1, player);
-                    board.splice(playerLocation, 1, black);
-                }
-
-                break;
-            }
-
-            case 'd': {
-                if (board[playerLocation + 1] === black) {
-                    board.splice(playerLocation + 1, 1, player);
-                    board.splice(playerLocation, 1, black);
-                }
-
-                if (board[playerLocation + 1] === block && board[playerLocation + 2] !== red) {
-                    if (board[playerLocation + 2] === goal) win = true;
-    
-                    board.splice(playerLocation + 2, 1, block);
-                    board.splice(playerLocation + 1, 1, player);
-                    board.splice(playerLocation, 1, black);
-                }
-
-                break;
-            }
-
-            case 'W': {
-                if (board[playerLocation + 10] === block && board[playerLocation - 10] === black) {
-                    board.splice(playerLocation - 10, 1, player);
-                    board.splice(playerLocation, 1, block);
-                    board.splice(playerLocation + 10, 1, black);
-                }
-                
-                if (board[playerLocation - 10] === black) {
-                    board.splice(playerLocation - 10, 1, player);
-                    board.splice(playerLocation, 1, black);
-                }
-
-                break;
-            }
-
-            case 'A': {
-                if (board[playerLocation + 1] === block && board[playerLocation - 1] === black) {
-                    board.splice(playerLocation - 1, 1, player);
-                    board.splice(playerLocation, 1, block);
-                    board.splice(playerLocation + 1, 1, black);
-                }
-
-                if (board[playerLocation - 1] === black) {
-                    board.splice(playerLocation - 1, 1, player);
-                    board.splice(playerLocation, 1, black);
-                }
-
-                break;
-            }
-
-            case 'S': {
-                if (board[playerLocation - 10] === block && board[playerLocation + 10] === black) {
-                    board.splice(playerLocation + 10, 1, player);
-                    board.splice(playerLocation, 1, block);
-                    board.splice(playerLocation - 10, 1, black);
-                }
-
-                if (board[playerLocation + 10] === black) {
-                    board.splice(playerLocation + 10, 1, player);
-                    board.splice(playerLocation, 1, black);
-                }
-
-                break;
-            }
-
-            case 'D': {
-                if (board[playerLocation - 1] === block && board[playerLocation + 1] === black) {
-                    board.splice(playerLocation + 1, 1, player);
-                    board.splice(playerLocation, 1, block);
-                    board.splice(playerLocation - 1, 1, black);
-                }
-
-                if (board[playerLocation + 1] === black) {
-                    board.splice(playerLocation + 1, 1, player);
-                    board.splice(playerLocation, 1, black);
-                }
-
-                break;
-            }
-
-            case 'r': {
-                newGame();
-                break;
-            }
-
-            case 'c': {
-                levels.set(message.author.id, level++);
-                newGame();
-                break;
-            }
-
-            case 'q': {
-                games.delete(message.author.id);
-                break;
-            }
-        }
-
-        if (win === true) {
-            const embed = new MessageEmbed()
-                .setAuthor(`Level ${level}`)
-                .setDescription(board.join(''))
-                .setFooter('Enter c to continue or q to quit')
-                .setColor(0x77B255);
-
-            await gameMessage.edit(embed);
-            await message.delete();
-
-            return;
-        }
-
-        const embed = new MessageEmbed()
-            .setAuthor(`Level ${level}`)
-            .setDescription(board.join(''))
-            .setFooter('Enter direction (w, a, s, d) or r to reset');
-
-        await gameMessage.edit(embed);
+        const plays = message.content.toUpperCase().match(/[DLCQ]|(?<!N|Q)U|R(?!A)|(?<!I)G/g);
+        const start = games.get(message.author.id).join('');
+        const reset = JSON.stringify(games.get(message.author.id));
         await message.delete();
+
+        plays.forEach(async (play, index) => {
+            setTimeout(async () => {
+                const board = games.get(message.author.id);
+                const grabbed = grabbers.get(message.author.id);
+                const gameMessage = messages.get(message.author.id);
+                const playerLocation = board.indexOf(player) === -1 ? board.indexOf(grabbing) : board.indexOf(player);
+
+                let moves = counters.get(message.author.id);
+                let level = levels.get(message.author.id);
+                let valid = false;
+                let win = false;
+
+                switch (play) {
+                    case 'U': {
+                        if (board[playerLocation - 10] === black) {
+                            if (board[playerLocation + 10] === block && grabbed === true) {
+                                grabbers.set(message.author.id, false);
+
+                                board.splice(playerLocation - 10, 1, player);
+                                board.splice(playerLocation, 1, block);
+                                board.splice(playerLocation + 10, 1, black);
+                            
+                            } else {
+                                board.splice(playerLocation - 10, 1, player);
+                                board.splice(playerLocation, 1, black);
+                            }
+                        
+                        }
+
+                        if (board[playerLocation - 10] === block && board[playerLocation - 20] !== red) {
+                            if (board[playerLocation - 20] === goal) win = true;
+
+                            board.splice(playerLocation - 20, 1, block);
+                            board.splice(playerLocation - 10, 1, player);
+                            board.splice(playerLocation, 1, black);
+                        }
+
+                        valid = true;
+                        moves++;
+                        break;
+                    }
+
+                    case 'D': {
+                        if (board[playerLocation + 10] === black) {
+                            if (board[playerLocation - 10] === block && grabbed === true) {
+                                grabbers.set(message.author.id, false);
+
+                                board.splice(playerLocation + 10, 1, player);
+                                board.splice(playerLocation, 1, block);
+                                board.splice(playerLocation - 10, 1, black);
+                            
+                            } else {
+                                board.splice(playerLocation + 10, 1, player);
+                                board.splice(playerLocation, 1, black);
+                            }
+                        }
+
+                        if (board[playerLocation + 10] === block && board[playerLocation + 20] !== red) {
+                            if (board[playerLocation + 20] === goal) win = true;
+            
+                            board.splice(playerLocation + 20, 1, block);
+                            board.splice(playerLocation + 10, 1, player);
+                            board.splice(playerLocation, 1, black);
+                        }
+
+                        valid = true;
+                        moves++
+                        break;
+                    }
+
+                    case 'L': {
+                        if (board[playerLocation - 1] === black) {
+                            if (board[playerLocation + 1] === block && grabbed === true) {
+                                grabbers.set(message.author.id, false);
+
+                                board.splice(playerLocation - 1, 1, player);
+                                board.splice(playerLocation, 1, block);
+                                board.splice(playerLocation + 1, 1, black);
+                            
+                            } else {
+                                board.splice(playerLocation - 1, 1, player);
+                                board.splice(playerLocation, 1, black);
+                            }
+                        }
+
+                        if (board[playerLocation - 1] === block && board[playerLocation - 2] !== red) {
+                            if (board[playerLocation - 2] === goal) win = true;
+            
+                            board.splice(playerLocation - 2, 1, block);
+                            board.splice(playerLocation - 1, 1, player);
+                            board.splice(playerLocation, 1, black);
+                        }
+
+                        valid = true;
+                        moves++
+                        break;
+                    }
+
+                    case 'R': {
+                        if (board[playerLocation + 1] === black) {
+                            if (board[playerLocation - 1] === block && grabbed === true) {
+                                grabbers.set(message.author.id, false);
+
+                                board.splice(playerLocation + 1, 1, player);
+                                board.splice(playerLocation, 1, block);
+                                board.splice(playerLocation - 1, 1, black);
+                            
+                            } else {
+                                board.splice(playerLocation + 1, 1, player);
+                                board.splice(playerLocation, 1, black);
+                            }
+                        }
+
+                        if (board[playerLocation + 1] === block && board[playerLocation + 2] !== red) {
+                            if (board[playerLocation + 2] === goal) win = true;
+            
+                            board.splice(playerLocation + 2, 1, block);
+                            board.splice(playerLocation + 1, 1, player);
+                            board.splice(playerLocation, 1, black);
+                        }
+
+                        valid = true;
+                        moves++
+                        break;
+                    }
+
+                    case 'G': {
+                        grabbers.set(message.author.id, true);
+                        board.splice(playerLocation, 1, grabbing);
+
+                        valid = true;
+                        moves++
+                        break;
+                    }
+
+                    case 'C': {
+                        const newLevel = level + 1;
+                        await newGame(newLevel);
+                        break;
+                    }
+
+                    case 'Q': {
+                        games.delete(message.author.id);
+                        messages.delete(message.author.id);
+                        levels.delete(message.author.id);
+                        break;
+                    }
+                }
+
+                if (valid === true && win === false) {
+                    counters.set(message.author.id, moves);
+                    const embed = new discord.MessageEmbed()
+                        .setAuthor(`Level ${level}`)
+                        .setDescription(board.join(''))
+                        .setFooter(`Number of moves: ${moves}`);
+
+                    await gameMessage.edit(embed);
+                }
+            
+                if (win === true) {
+                    const embed = new discord.MessageEmbed()
+                        .setAuthor(`Level ${level}`)
+                        .setDescription(board.join(''))
+                        .setFooter(`Number of moves: ${moves}`)
+                        .setColor(0x77B255);
+
+                    await gameMessage.edit(embed);
+                }
+
+                setTimeout(async () => {
+                    if ((index + 1) === plays.length && valid === true && win === false) {
+                        games.set(message.author.id, JSON.parse(reset));
+                        counters.set(message.author.id, 0);
+
+                        const embed = new discord.MessageEmbed()
+                            .setAuthor(`Level ${level}`)
+                            .setDescription(start)
+                            .setFooter('Number of moves: 0');
+
+                        await gameMessage.edit(embed);
+                    }
+
+                }, 1000);
+
+            }, index * 1500);
+        });
     }
 });
 
