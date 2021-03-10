fb22de6008e99b8d5b48c5567a0316b394cccf63
Ellie-exe <ryiscool247@gmail.com>
2021-03-10 16:57:00 -0500

    Removed message controls

--- src/index.js
+++ src/index.js
@@ -12,44 +12,10 @@ client.on('message', async (message) => {
     const args = message.content.slice(process.env.prefix.length).trim().split(/ +/);
     const command = args.shift().toLowerCase();
 
-    if (message.author.bot) return;
-
-    if (cache.has(message.author.id)) {
-        const direction = /(?<!\w)(U(?=P\s)|D(?=OWN\s)|L(?=EFT\s)|R(?=IGHT\s)|P(?=ULL\s(UP\s|DOWN\s|LEFT\s|RIGHT\s)))/g;
-        const input = message.content.toLocaleUpperCase();
-
-        let moves = input.match(/(?<!\w)Q(?=UIT)/g);
-        if (moves === null) moves = input.match(/(?<!\w)C(?=ONTINUE)/g);
-        if (moves === null) moves = input.match(direction);
-        if (moves === null) moves = input.match(/[Q]/g);
-        if (moves === null) moves = input.match(/[C]/g);
-        if (moves === null) moves = input.match(/[UDLRP]/g);
-        if (moves === null) return;
-
-        if (!message.channel.messages.cache.has(cache.get(message.author.id).messageID)) return;
-
-        if (moves.includes('Q')) {
-            cache.del(message.author.id);
-            return;
-        }
-
-        await message.delete();
-
-        if (moves.includes('C')) {
-            game.generate(message, cache, cache.get(message.author.id).level + 1);
-            return;
-        }
-
-        game.move(message, message.author, cache, moves);
-        return;
-    }
-
-    if (!message.content.startsWith(process.env.prefix)) return;
+    if (!message.content.startsWith(process.env.prefix) || message.author.bot) return;
     logger.info(`${message.channel.id} ${message.author.tag}: ${process.env.prefix}${command}`);
 
-    if (command === 'play') {
-        game.generate(message, cache);
-    }
+    if (command === 'play') game.generate(message, cache);
 });
 
 client.on('messageReactionAdd', async (reaction, user) => {
